42f97110c0dad268986f8db33af0c5ede3b0d5b8
azmianshari <azmianshari123@gmail.com>
2023-06-23 10:57:51 +0700

    Mengecek jika antrian kosong

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -58,6 +58,14 @@ public:
 	void display() {
 		int FRONT_position = FRONT;
 		int REAR_position = REAR;
+
+		//Cek jika antrian kosong.
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+		
+	
 	}
 	
 };
