adae7b62805069e5028e9dc38bd3903da7ff7bb3
azmianshari <azmianshari123@gmail.com>
2023-06-23 09:41:30 +0700

    Mengecek jika antrian hanya memiliki 1 elemen

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -40,5 +40,13 @@ public:
 			cout << "Queue underflow\n";
 			return;
 		}
+		cout << "\nThe element deleted from the queue is :" << queue_array[FRONT] << "\n";
+
+		//Cek jika antrian hanya memiliki satu elemen.
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		
 	}
 };
